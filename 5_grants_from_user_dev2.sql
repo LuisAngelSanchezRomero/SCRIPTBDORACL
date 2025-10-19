265195eabb797a6ed69eaa53172b9e6d068c872e
LuisSánchez <luis.sanchez.romero@vallegrande.edu.pe>
2025-10-19 12:45:49 -0500

    feat: Update sql script grants

--- 5_grants_from_user_dev2.sql
+++ 5_grants_from_user_dev2.sql
@@ -3,8 +3,9 @@
 -- ============================================
 -- GRANTS: Permisos para que ANDRE_DEV pueda referenciar/consultar y acceder a mis tablas.
 -- ============================================
-GRANT SELECT, REFERENCES ON product TO ANDRE_DEV;
-GRANT SELECT, REFERENCES ON inventory_movement TO ANDRE_DEV;
-GRANT SELECT, REFERENCES ON report TO ANDRE_DEV;
-GRANT SELECT, REFERENCES ON report_detail TO ANDRE_DEV;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON product TO ANDRE_DEV;
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON inventory_movement TO ANDRE_DEV;
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON report TO ANDRE_DEV;
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON report_detail TO ANDRE_DEV;
 COMMIT;
